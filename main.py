f61b6f512d11df1fdab4590143262ec1c469a0c2
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-11-25 13:36:44 +0330

    installing git

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from pyaction.workflow.stream import WorkflowContext
 workflow = PyAction()
 
 
-def get_pushed_articles() -> List[str]:
+def get_pushed_articles(ghb, sha) -> List[str]:
     try:
         # Run the Git command and capture the output
         AN.notice("Getting the published/modified article.")
         result = subprocess.run(
-            ["git", "diff", "--name-only", "HEAD", "HEAD~1"],
+            ["git", "--no-pager", "diff", "--name-only", sha, ghb],
             text=True,  # Ensure output is in string format
             capture_output=True,
             check=True,  # Raise an error if the command fails
@@ -22,13 +22,14 @@ def get_pushed_articles() -> List[str]:
         changed_files = result.stdout.strip().split("\n")
         return changed_files
     except subprocess.CalledProcessError as e:
-        AN.error(f"Unable to find the article: {e}")
+        AN.error(f"Unable to find the article: {e.stderr}")
         raise SystemExit(1)
 
 
 @workflow.action
-def action() -> None:
-    article_path: str = get_pushed_articles()[0]
+def action(github_event_before: str, github_sha: str) -> None:
+    AN.notice(f"GitHub Event Before: {github_event_before}, SHA: {github_sha}")
+    article_path: str = get_pushed_articles(github_event_before, github_sha)[0]
 
     with open(article_path, "r") as article:
         content: str = article.read()
